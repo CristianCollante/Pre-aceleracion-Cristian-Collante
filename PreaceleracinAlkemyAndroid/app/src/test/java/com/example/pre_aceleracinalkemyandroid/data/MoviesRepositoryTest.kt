3e497d8b92f7994b141945793e216d494276b96e
CristianCollante <profcristiancollante@gmail.com>
2022-09-18 22:37:13 -0300

    Implementando Test

---
+++ PreaceleracinAlkemyAndroid/app/src/test/java/com/example/pre_aceleracinalkemyandroid/data/MoviesRepositoryTest.kt
@@ -0,0 +1,31 @@
+package com.example.pre_aceleracinalkemyandroid.data
+
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+import org.mockito.Mock
+
+
+class MoviesRepositoryTest{
+
+    @Mock private lateinit var moviesRemoteDataSource: MoviesRemoteDataSource
+
+    private lateinit var moviesRepository: MoviesRepository
+
+    @Before
+    fun onBefore(){
+        moviesRepository = MoviesRepository(moviesRemoteDataSource)
+    }
+
+
+    @Test
+    fun getDetails() = runBlocking{
+        
+
+    }
+
+
+}
+
+
